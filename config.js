1140be912cd3d5373ecfbb6bc7820dd3c86016d4
httaX995 <httx995@gmail.com>
2025-07-07 18:36:08 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,8 +5,8 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "kvlCGA7B#W0dEyiwD2jYYLxETAptBnL4b7M2DHb_ulYCXZmVPRxE", // session id
-AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true", // true,false
+SESSION_ID: process.env.SESSION_ID || "L9hHGTDC#TlS7Rn9ERwSS3-CmZkLF-Npl8V_ikiYSG8ajHQnclaU", // session id
+AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "falae", // true,false
 ANTI_DELETE: process.env.ANTI_DELETE || "true", // true,false
 MODE: process.env.MODE || "public", // public,private,groups,inbox
 ALLWAYS_OFFLINE: process.env.ALLWAYS_OFFLINE || "false", // true,false
